1b849e63d1f337d7910843e2123a41153916b0c7
liupenghao <anhusky@163.com>
2023-05-31 18:09:18 +0800

    feat: 项目构建弹窗页面

---
+++ src/components/project/ProjectBuildPop.vue
@@ -0,0 +1,36 @@
+<template>
+  <Modal v-model="projectBuildPopVisible" title="编辑" @on-ok="ok()" @on-cancel="cancel()">
+    <Form ref="repository"  :label-width="80">
+      <FormItem label="名称:" prop="name">
+        <Input type="text" />
+      </FormItem>
+
+    </Form>
+  </Modal>
+</template>
+
+
+<script>
+
+export default {
+  props: {
+    project: {
+      type: Object,
+      default: {}
+    },
+    projectBuildPopVisible: {
+      type: Boolean,
+      default: false
+    }
+
+  },
+  methods: {
+    ok() {
+      this.$emit('project-build-save')
+    },
+    cancel() {
+      this.$emit('project-build-cancel')
+    },
+  }
+}
+</script>
